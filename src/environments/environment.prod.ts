bdc44f3855b5a2690ada6e93c052f1392ffa44f5
dhaval-intelehealth <dhaval@intelehealth.org>
2024-12-02 00:28:53 +0530

    fix: env issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
   notificationURL: `${window.location.protocol}//${window.location.hostname}:3004/notification`,
   socketURL: `${window.location.protocol}//${window.location.hostname}:3004`,
   webrtcSdkServerUrl: `wss://${window.location.hostname}:9090`,
-  webrtcTokenServerUrl: `${window.location.protocol}//${window.location.hostname}:3000`,
+  webrtcTokenServerUrl: `${window.location.protocol}//${window.location.hostname}:3000/`,
   captchaSiteKey: "6LdowFQmAAAAADsQdI3gtUM88w4u5wV5uQLkvoSo",
   firebase: {
     /* apiKey: "AIzaSyC5cRqdDtLWwJpz7WY1Ekpx7rbawbG1CA8",
