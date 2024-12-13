6bb7e23600f78f2be340ec9254e65a4ffb001c3f
Zeeshan <zeeshan@intelehealth.org>
2024-12-13 15:06:57 +0530

    version code updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,6 +9,6 @@ export const environment = {
   gatewayURL: `${window.location.protocol}//${window.location.hostname}:3030/`,
   webrtcSdkServerUrl: `wss://${window.location.hostname}:9090`,
   webrtcTokenServerUrl: `${window.location.protocol}//${window.location.hostname}:3000/`,
-  version: "NAS-v1.3.1",
-  versionCode: "28"
+  version: "NAS-v1.3.2",
+  versionCode: "29"
 };
