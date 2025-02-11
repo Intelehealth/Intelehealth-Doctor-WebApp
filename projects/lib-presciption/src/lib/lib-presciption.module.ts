e0f83a8c0ba87389223b130e92333aac472ad516
Vishal Majumdar <vishal.majumdar958@gmail.com>
2025-02-11 17:24:47 +0530

    feat: added the initial setup for prescription pluggable feature

---
+++ projects/lib-presciption/src/lib/lib-presciption.module.ts
@@ -0,0 +1,16 @@
+// import { NgModule } from '@angular/core';
+// import { LibPresciptionComponent } from './lib-presciption.component';
+
+
+
+// @NgModule({
+//   declarations: [
+//     LibPresciptionComponent
+//   ],
+//   imports: [
+//   ],
+//   exports: [
+//     LibPresciptionComponent
+//   ]
+// })
+// export class LibPresciptionModule { }
---
+++ projects/lib-presciption/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of lib-presciption
+ */
+
+export * from './lib/lib-presciption.service';
+export * from './lib/lib-presciption.component';
+// export * from './lib/lib-presciption.module';
